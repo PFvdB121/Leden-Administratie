a360bbd3f7b3a2dcb1dcffa9fed5db8041fba52c
PFvdB121 <patriquevdboom@gmail.com>
2023-11-16 12:03:10 +0100

    Fixed user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('naam');
-            $table->string('emailadres')->unique();
+            $table->string('name');
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('wachtwoord');
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
